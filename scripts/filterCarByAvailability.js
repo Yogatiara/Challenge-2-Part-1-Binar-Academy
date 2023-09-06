f3890c98c326f4d955da764b9959e34a29a0886c
yoga <11211086@student.itk.ac.id>
2023-09-06 13:53:26 +0800

    adding logic filter, asc sort, and dsc sort

--- scripts/filterCarByAvailability.js
+++ scripts/filterCarByAvailability.js
@@ -1,12 +1,20 @@
+// const dataMobil = require('../data/mobil') --testing
+
 function filterCarByAvailability(cars) {
-  // Sangat dianjurkan untuk console.log semua hal hehe
-  console.log(cars);
 
   // Tempat penampungan hasil
   const result = [];
+  // console.log(cars[0].available) --> testing
 
   // Tulis code-mu disini
+  for (let i = 0; i < cars.length; i++) {
+    if (cars[i].available === true) {
+      result.push(cars[i]);
+    }
+  }
 
   // Rubah code ini dengan array hasil filter berdasarkan availablity
   return result;
 }
+
+// console.log(filterCarByAvailability(dataMobil.dataMobil)) --> testing
--- scripts/sortCarByYearAscendingly.js
+++ scripts/sortCarByYearAscendingly.js
@@ -1,13 +1,27 @@
+const dataMobil = require('../data/mobil')
+
 function sortCarByYearAscendingly(cars) {
   // Sangat dianjurkan untuk console.log semua hal hehe
-  console.log(cars);
+  // console.log(cars);
 
   // Clone array untuk menghindari side-effect
   // Apa itu side effect?
   const result = [...cars];
-
   // Tulis code-mu disini
 
+  for (let x = 0; x < result.length; x++) {
+    for (let y = 0; y < result.length - 1; y++) {
+      if (result[y + 1].year < result[y].year) {
+        if (result[y + 1].year == result[y].year) {
+          continue
+        }
+        [result[y], result[y + 1]] = [result[y + 1], result[y]];
+      }
+    }
+  }
   // Rubah code ini dengan array hasil sorting secara ascending
   return result;
 }
+
+sortCarByYearAscendingly(dataMobil.dataMobil)
+
